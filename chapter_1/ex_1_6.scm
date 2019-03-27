f362df0793f1cb1d34801d15abc76e63c1da3480
paivett <santiago.pivetta@gmail.com>
2019-03-27 20:30:30 -0300

    Exercise 1.6

---
+++ chapter_1/ex_1_6.scm
@@ -0,0 +1,50 @@
+; (define (sqrt-iter guess x)
+;     (if (good-enough? guess x)
+;         guess
+;         (sqrt-iter (improve guess x) x)
+;     )
+; )
+
+(define (square x)
+    (* x x)
+)
+
+(define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001)
+)
+
+(define (improve guess x)
+    (average guess (/ x guess))
+)
+
+(define (average x y)
+    (/ (+ x y) 2)
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(define (new-if predicate then-clause else-clause)
+    (cond (predicate then-clause)
+          (else else-clause))
+)
+
+(define (sqrt-iter guess x)
+    (new-if (good-enough? guess x)
+            guess
+            (sqrt-iter (improve guess x) x)
+    )
+)
+
+; The problem is that the new-if is now a procedure, thus the interpreter first evaluates
+; the operator, and the arguments, and then tries to apply the combination, but the second
+; argument is a call to itself, generating an infinite chain of calls.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(define (asd x)
+    (sqrt-iter 1.0 x)
+)
