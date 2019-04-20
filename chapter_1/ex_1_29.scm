3f50ac6c1e7920fdd013d3b138db2d6b62f55cee
paivett <santiago.pivetta@gmail.com>
2019-04-20 17:43:03 -0300

    Exercise 1.29

---
+++ chapter_1/ex_1_29.scm
@@ -0,0 +1,33 @@
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (simpson-integral f a b n)
+    (define (h)
+        (/ (- b a) n))
+    
+    (define (yk-coef k)
+        (cond ((or (= k 0) (= k n)) 1)
+              ((even? k) 2)
+              (else 4))
+    )
+
+    (define (yk k)
+        (+ a (* k (h))))
+    
+    (define (aux k)
+        (* (yk-coef k) (f (yk k))))
+
+    (define (inc x) (+ x 1))
+
+    (* (/ (h) 3) (sum aux 0.0 inc n))
+)
+
+(define (cube x) (* x x x))
+
+; (simpson-integral cube 0 1 100) = .24999999999999992
+; (simpson-integral cube 0 1 1000) = .2500000000000003
+
+; Simpsons rule seems to be more accurate that the original method
