2c906796572a78c363b0d1d386bb1c91b5142101
paivett <santiago.pivetta@gmail.com>
2019-06-25 21:04:51 -0300

    Exercise 2.15

---
+++ chapter_2/ex_2_15.scm
@@ -0,0 +1,3 @@
+; She is right! If the same interval is repeated, then there is a new operation and the uncertainty is changed again
+; so it is better to write the computation with the less operations as possible, or with the operations that keep
+; the uncertainty as low as possible
