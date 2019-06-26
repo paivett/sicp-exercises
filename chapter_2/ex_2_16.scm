fe490c7b37e90d5b49509505a1d7d13d464d43a6
paivett <santiago.pivetta@gmail.com>
2019-06-25 21:05:04 -0300

    Exercise 2.16

---
+++ chapter_2/ex_2_16.scm
@@ -0,0 +1,5 @@
+; As explained in previous exercises, the rules of algebra for real numbers does not apply when dealing with these intervals
+; which represent a real with an error associated. The uncertainty, or error, changes with every operation performed, so
+; it is impossible to come up with a computation way that is inmune to this. The person writing this formulations must be
+; aware of this fact and try to write the computation in a way that keeps the error to a minimum. This is just as multiplying
+; matrices or vectors, for example, where product (or cross=product) is not conmutative.
