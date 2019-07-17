4da0050aad825464a7d8e1745e38bcc420c76fc0
paivett <santiago.pivetta@gmail.com>
2019-07-16 21:24:24 -0300

    Exercise 2.36

---
+++ chapter_2/ex_2_36.scm
@@ -0,0 +1,15 @@
+(define nil (list))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (accumulate cons nil (map car seqs)))
+            (accumulate-n op init (accumulate cons nil (map cdr sequences))))))
+
+(define s (list (list 1 2 3) (list 4 5 6)))
