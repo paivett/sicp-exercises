60a45182aa1d7a61799fd676a89fa1f0617ee1ce
paivett <santiago.pivetta@gmail.com>
2019-08-19 20:21:18 -0300

    Exercise 2.51

---
+++ chapter_2/ex_2_51.scm
@@ -0,0 +1,20 @@
+; Version 1
+(define (below painter1 painter2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-bottom
+           (transform-painter painter1
+                              (make-vect 0.0 0.0)
+                              (make-vect 0.0 1.0)
+                              split-point))
+          (paint-top
+           (transform-painter painter2
+                              split-point
+                              (make-vect 1.0 0.5)
+                              (make-vect 0.0 1.0))))
+      (lambda (frame)
+        (paint-bottom frame)
+        (paint-top frame)))))
+
+; Version using rotations
+(define (below-rot painter1 painter2)
+  (rotate90 (beside (rotate270 painter1) (rotate270 painter2))))
