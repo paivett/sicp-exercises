06a82fb1de326945e150e8a04a7bcfe1d9e3f8fd
paivett <santiago.pivetta@gmail.com>
2019-08-31 20:41:06 -0300

    Exercise 2.60

---
+++ chapter_2/ex_2_60.scm
@@ -0,0 +1,19 @@
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set) (cons x set))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)        
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+(define (union-set set1 set2) (append set1 set2))
+
+; This representation would be useful, compared to the unordered list of non repeated
+; elements, if the most used operations were adjoin-set and union-set since they have
+; time complexity of O(1) and O(n) respectively.
