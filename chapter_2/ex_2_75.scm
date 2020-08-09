beddc1fbafc6fb2f96f9dcbbbd68eac3956560bf
paivett <santiago.pivetta@gmail.com>
2020-08-08 22:07:54 -0300

    Complete exercise 2.75 so it can be executed

--- chapter_2/ex_2_75.scm
+++ chapter_2/ex_2_75.scm
@@ -1,3 +1,25 @@
+#lang sicp
+
+(define (apply-generic op arg) (arg op))
+
+(define (square x) (* x x))
+
+(define (real-part z) (apply-generic 'real-part z))
+(define (imag-part z) (apply-generic 'imag-part z))
+(define (magnitude z) (apply-generic 'magnitude z))
+(define (angle z) (apply-generic 'angle z))
+
+(define (make-from-real-imag x y)
+  (define (dispatch op)
+    (cond ((eq? op 'real-part) x)
+          ((eq? op 'imag-part) y)
+          ((eq? op 'magnitude)
+           (sqrt (+ (square x) (square y))))
+          ((eq? op 'angle) (atan y x))
+          (else
+           (error "Unknown op -- MAKE-FROM-REAL-IMAG" op))))
+  dispatch)
+
 (define (make-from-mag-ang m a)
   (define (dispatch op)
     (cond ((eq? op 'real-part ) (* m (cos a)))
